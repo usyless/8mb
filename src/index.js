d6698c55630ce205e9f494e7f440341183306ad3
usy <contact.usyless@gmail.com>
2025-08-22 23:13:45 +0100

    progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import { FFmpeg } from '@ffmpeg/ffmpeg';
 
 let onProgress;
@@ -6,7 +8,6 @@ const getFFmpeg = (() => {
     const baseURL = (window.crossOriginIsolated) ? './ffmpeg/' : './ffmpeg-mt/';
 
     const ffmpeg = new FFmpeg();
-    let loaded = false;
 
     ffmpeg.on('log', ({ message }) => {
         console.log(message);
@@ -17,7 +18,7 @@ const getFFmpeg = (() => {
     });
 
     return async () => {
-        if (!loaded) {
+        if (!ffmpeg.loaded) {
             try {
                 const loadData = {
                     coreURL: baseURL + 'ffmpeg-core.js',
@@ -27,13 +28,45 @@ const getFFmpeg = (() => {
                     loadData.workerURL = baseURL + 'ffmpeg-core.worker.js';
                 }
                 await ffmpeg.load(loadData);
-                loaded = true;
             } catch (error) {
-                loaded = false;
                 console.error(error);
+                throw error;
             }
-        } else {
-            return ffmpeg;
         }
+        return ffmpeg;
     }
 })();
+
+/** @type {HTMLInputElement} */
+const fileInput = document.getElementById('file');
+
+fileInput.addEventListener('change', (e) => {
+    const files = fileInput.files;
+    fileInput.disabled = true;
+
+    getFFmpeg().then(async (ffmpeg) => {
+        for (const file of files) {
+            const wroteFile = await ffmpeg.writeFile('input', new Uint8Array(await file.arrayBuffer()));
+
+            if (!wroteFile) {
+                // error
+                continue;
+            }
+
+            onProgress = (progress, time) => {
+                console.log(`progress: ${progress}, time: ${time}`);
+            };
+
+            const status = await ffmpeg.exec(['-i', 'input', 'output.mp4']);
+
+            if (status !== 0) {
+                // error
+                continue;
+            }
+        }
+    }).catch((e) => {
+        // display error
+        fileInput.disabled = false;
+        onProgress = null;
+    });
+});
