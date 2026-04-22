b4185560ed4c4fa53181b6e4f3b3c1cef2cc88a4
usy <contact.usyless@gmail.com>
2026-04-22 11:15:16 +0100

    -threads

--- src/index.js
+++ src/index.js
@@ -434,6 +434,7 @@ fileInput.addEventListener('change', async () => {
             '-maxrate', videoBitrate.toString(),
             '-c:a', 'aac',
             '-b:a', audioBitrate.toString(),
+            '-threads', navigator.hardwareConcurrency.toString(), // 4 for chromium, unset for firefox?
             outputFileName
         ];
         console.log("Ffmpeg command parameters:", ffmpegParameters);
