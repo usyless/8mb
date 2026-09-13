67d552f18209ae4b23c6a084c610296b9eb12fa3
usy <contact.usyless@gmail.com>
2026-09-13 01:29:07 +0100

    fix service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,11 +9,8 @@ const contentToCache = [
     './styles.css',
     './shared.css',
     './popup.css',
-    './index.js',
-    './popups.js',
-    './ffmpeg.js',
-    './822.ffmpeg.js',
-    './updater.js',
+    './bundle.js',
+    './worker.js',
 
     './8mb.webmanifest',
 
